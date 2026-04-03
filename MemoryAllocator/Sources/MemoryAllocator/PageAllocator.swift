f2aa62de5cfe6e4031a6aecdc654bfbd4b98ca28
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 14:06:14 +0100

    Create constructor + deconstructor.

--- MemoryAllocator/Sources/MemoryAllocator/PageAllocator.swift
+++ MemoryAllocator/Sources/MemoryAllocator/PageAllocator.swift
@@ -1,4 +1,22 @@
 import Foundation
 
 struct PageAllocator: ~Copyable {
+    let size: Int
+
+    let pointer: UnsafeMutableRawPointer
+
+    init?(size: Int) {
+        let _pointer = MemoryAllocator.requestMemory(of: size)
+
+        guard let _pointer = try? _pointer.get() else {
+            return nil
+        }
+
+        self.size = size
+        pointer = _pointer
+    }
+
+    deinit {
+        let result = munmap(pointer, size)
+    }
 }
