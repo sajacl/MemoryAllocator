739bb1b81fe3a59ad37c4f3648f4653b6d01250b
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 14:04:48 +0100

    Introduce `RequestMemoryFailure`.

--- MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
+++ MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
@@ -2,4 +2,19 @@ import Foundation
 
 enum MemoryAllocator {
 
+    struct RequestMemoryFailure: LocalizedError {
+        let error: Int32?
+
+        init(error: Int32?) {
+            self.error = error
+        }
+
+        var localizedDescription: String {
+            return "Failed to allocate memory"
+        }
+
+        var failureReason: String? {
+            nil
+        }
+    }
 }
