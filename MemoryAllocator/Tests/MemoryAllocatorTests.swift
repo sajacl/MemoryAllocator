21d24aaaf06b2981cb9a8b3a478a5d4e40d69c65
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 15:31:56 +0100

    Add `pageAllocator_multiplePages`.

--- MemoryAllocator/Tests/MemoryAllocatorTests.swift
+++ MemoryAllocator/Tests/MemoryAllocatorTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Testing
 @testable import MemoryAllocator
 
@@ -5,7 +6,7 @@ import Testing
 struct MemoryAllocatorTests {
     @Test
     func pageAllocator_general() throws {
-        let size = 10 // bytes
+        let size = 8 // bytes
 
         let page = try Memory.Page(size)
 
@@ -13,5 +14,27 @@ struct MemoryAllocatorTests {
         print(page.pointer)
 
         try page.free()
+    }
+
+    @Test
+    func pageAllocator_multiplePages() throws {
+        let size = 8 // bytes
+
+        var pointers: [UnsafeMutableRawPointer] = []
+
+        for i in 1...2 {
+            let page1 = try Memory.Page(size)
+
+            page1.pointer.storeBytes(of: i, as: Int.self)
+            #expect(i == page1.pointer.load(as: Int.self))
+
+            pointers.append(page1.pointer)
+        }
+
+        for pointer in pointers {
+            if munmap(pointer, size) == -1 {
+                throw Memory.FreeRequestMemoryFailure(error: errno)
+            }
+        }
     }
 }
