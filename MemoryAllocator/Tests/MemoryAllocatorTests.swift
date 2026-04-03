8892d3d1c41b4f9209710774ba47d6e213fb2ee8
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 14:38:44 +0100

    Add body to `pageAllocator_general` test method.

--- MemoryAllocator/Tests/MemoryAllocatorTests.swift
+++ MemoryAllocator/Tests/MemoryAllocatorTests.swift
@@ -4,7 +4,14 @@ import Testing
 @Suite
 struct MemoryAllocatorTests {
     @Test
-    func pageAllocator_general() {
-        
+    func pageAllocator_general() throws {
+        let size = 10 // bytes
+
+        let page = try Memory.Page(size)
+
+        print(page.size)
+        print(page.pointer)
+
+        page.free()
     }
 }
