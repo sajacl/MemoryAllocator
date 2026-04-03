3352b8140f9357b304ccf4be3ffc780148df2713
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 14:05:13 +0100

    Introduce `MemoryAllocator.requestMemory`.

--- MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
+++ MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
@@ -1,6 +1,26 @@
 import Foundation
 
 enum MemoryAllocator {
+    static func requestMemory(of size: Int) -> Result<UnsafeMutableRawPointer, Error> {
+        let pointer = mmap(
+            nil,
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANON,
+            -1,
+            0
+        )
+
+        if pointer == MAP_FAILED {
+            return .failure(RequestMemoryFailure(error: errno))
+        }
+
+        guard let pointer else {
+            return .failure(RequestMemoryFailure(error: nil))
+        }
+
+        return .success(pointer)
+    }
 
     struct RequestMemoryFailure: LocalizedError {
         let error: Int32?
