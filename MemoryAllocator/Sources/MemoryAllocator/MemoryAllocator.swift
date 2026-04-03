65aa93c5831a1d2e678fc494d79974148d7696e0
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-04-03 14:37:12 +0100

    Remove indirection of `Allocator`.

--- MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
+++ MemoryAllocator/Sources/MemoryAllocator/MemoryAllocator.swift
@@ -1,23 +1,31 @@
 import Foundation
 
+/// A wrapper that indicate calls passes through a syscall.
 extension Memory {
-    enum Allocator {
-        /// Method to request memory from OS, using `mmap`.
-        /// Providing generic arguments for mmap syscall.
-        /// - Parameter size: size of memory in bytes.
-        static func requestMemory(of size: Int) -> Result<UnsafeMutableRawPointer, Error> {
-            let pointer = mmap(
-                nil,
-                size,
-                PROT_READ | PROT_WRITE,
-                MAP_PRIVATE | MAP_ANON,
-                -1,
-                0
-            )
+    /// Method to request memory from OS, using `mmap`.
+    /// Providing generic arguments for mmap syscall.
+    /// - Parameter size: size of memory in bytes.
+    @inlinable
+    static func requestMemory(of size: Int) throws -> UnsafeMutableRawPointer {
+        let pointer = mmap(
+            nil,
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANON,
+            -1,
+            0
+        )
 
-            if pointer == MAP_FAILED {
-                return .failure(RequestMemoryFailure(error: errno))
-            }
+        if pointer == MAP_FAILED {
+            throw RequestMemoryFailure(error: errno)
+        }
+
+        guard let pointer else {
+            throw RequestMemoryFailure(error: nil)
+        }
+
+        return pointer
+    }
 
             guard let pointer else {
                 return .failure(RequestMemoryFailure(error: nil))
